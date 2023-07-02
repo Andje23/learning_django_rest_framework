6d3582633107bf7418a1e8fb39a0b3681c0ddde5
Andje23 <108631076+Andje23@users.noreply.github.com>
2023-07-02 22:28:22 +0300

    Understanding HTTP Status Codes
    Understanding HTTP Status Codes

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,6 +2,7 @@ from .models import Blog
 from .serializer import BlogSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
 
@@ -10,15 +11,15 @@ def blog_list(reguest):
     if reguest.method == "GET":
         all_blogs = Blog.objects.filter(is_public=True)
         serializer = BlogSerializer(all_blogs, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     if reguest.method == "POST":
         serializer = BlogSerializer(data=reguest.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DLETE'])
@@ -26,18 +27,18 @@ def blog_detail(request, pk):
     if request.method == "GET":
         blog = Blog.objects.get(is_publik=True, pk=pk)
         serializer = BlogSerializer(blog)
-        return  Response(serializer.data)
+        return  Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == "PUT":
         blog = Blog.objects.get(pk=pk)
         serializer = BlogSerializer(blog, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
     if request.method == "DELETE":
         blog = Blog.objects.get(pk=pk)
         blog.delete()
-        return Response()
+        return Response(status=status.HTTP_200_OK)
